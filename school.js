7d8550a29d9bc4ead45180c5aec32e989f6363a3
Nora Villaseñor <norafer1993@hotmail.com>
2018-04-20 20:59:41 -0500

    escuela

---
+++ school.js
@@ -0,0 +1,15 @@
+let edad = 100;
+if(edad>=3 && edad<=6){
+    console.log("esta en kinder");
+}
+if (edad>6 && edad<=12){
+    console.log("esta en primaria")
+}
+if (edad>12 && edad<=15){
+    console.log("esta en secundaria");
+}
+
+
+
+
+
