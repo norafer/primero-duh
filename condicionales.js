24defdb195dcbc7b2d2ffd993e88f376e8af5266
Nora Villaseñor <norafer1993@hotmail.com>
2018-04-20 20:55:02 -0500

    condicionales.js

---
+++ condicionales.js
@@ -0,0 +1,25 @@
+let edad = 0;
+if (edad>=18){
+    console.log("ya es mayor de edad");
+    if(edad>=40 & edad<=60) {
+        console.log("podria ser tu jefe");
+    }
+    else if (edad>60 & edad <=90){
+        console.log("podria ser tu abuelo");
+    }
+    else{
+        console.log("neta no")
+    }
+}
+else {
+    console.log("esta morro");
+    if(edad>=15 & edad<=18){
+        console.log("esta en la prepa")
+     }
+     else if(edad>=12 && edad<15) {
+         console.log("esta en la secundaria")
+    
+     }
+    
+
+}
