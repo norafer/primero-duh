6cbb3a130c571681aa4547004de3cc803f231955
Nora Villaseñor <norafer1993@hotmail.com>
2018-04-24 21:50:27 -0500

    con j quory

--- 1html.js
+++ 1html.js
@@ -1,27 +1,22 @@
 
 
 let cinta_blanca =[
-{
+    {
 "nombre":"pancho",
 "edad":23
-},
-{
+    },
+    {
 "nombre":"juan",
 "edad":24
-},
-{
+    },
+    {
 "nombre":"juan",
 "edad":24
-}
+    }
 ]   
 for(i=0; i<cinta_blanca.length; i++){
-  let h1= document.createElement("h1");
-  let texto= document.createTextNode("este wey se llama "
-  +cinta_blanca[i].nombre+ " y tiene " +cinta_blanca[i].edad+
-   " años");
-   h1.appendChild(texto);
-   elBody.appendChild(h1);
-
+let h1=$("<h1>"+" este wey se llama "+cinta_blanca[i].nombre+ " y tiene"+cinta_blanca[i].edad+ "años"+ "</h1>");
+ $("#elBody").append(h1);
 }
 
 
